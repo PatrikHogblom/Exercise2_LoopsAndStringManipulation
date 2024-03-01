6121f9bb6c5520862429b5be44e44c5a235d9bb5
PatrikHogblom <patrik.hogblom@live.se>
2024-03-01 10:07:21 +0100

    skapat huvudmeny

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //huvudmeny
+            //skapa det så det håller sig vid liv
+            bool programRun = true;
+
+            while (programRun == true)
+            {
+                //säg till användaren att de har kommit till huvudmenyn
+                Console.WriteLine("välkommen till huvudmenyn, choose one of the options below.");
+                Console.WriteLine("0. Exits the program");
+                string inputOption = Console.ReadLine().ToLower();
+
+                switch (inputOption)
+                {
+                    case "0": //stänger programmet
+                        Console.WriteLine("Exits the program");
+                        programRun = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            
+            }
+
+
         }
     }
 }
