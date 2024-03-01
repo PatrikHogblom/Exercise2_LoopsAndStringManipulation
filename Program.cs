a975e427321bff3bece9c024211b019d0aa2a9f7
PatrikHogblom <patrik.hogblom@live.se>
2024-03-01 11:18:38 +0100

    case 1: addera 1 person färdigt

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercise2_LoopsAndStringManipulation
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise2_LoopsAndStringManipulation
 {
     internal class Program
     {
@@ -13,13 +15,18 @@
                 //säg till användaren att de har kommit till huvudmenyn
                 Console.WriteLine("välkommen till huvudmenyn, choose one of the options below.");
                 Console.WriteLine("0. Exits the program");
+                Console.WriteLine("1. Add person");
                 string inputOption = Console.ReadLine().ToLower();
 
                 switch (inputOption)
                 {
+
                     case "0": //stänger programmet
                         Console.WriteLine("Exits the program");
                         programRun = false;
+                        break;
+                    case "1"://addera person baserad på ålder
+                        AddPerson();
                         break;
                     default:
                         Console.WriteLine("Invalid input");
@@ -27,8 +34,17 @@
                 }
             
             }
-
-
         }
+
+        public static void AddPerson()
+        {
+            Console.WriteLine("Input your age: ");
+            uint.TryParse(Console.ReadLine(), out uint age);
+
+            Person person = new Person(age);
+            Console.WriteLine(person.getPriceOfPersonMessage());
+            //Console.ReadLine();
+        }
+
     }
 }
---
+++ person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_LoopsAndStringManipulation
+{
+
+    internal enum Prices 
+    {
+        standard = 120,
+        youth = 80,
+        senior = 90
+    }
+
+    internal class Person
+    {
+        public uint age;
+        public uint price;
+
+        public Person() { }
+        public Person(uint age)
+        {
+            this.age = age;
+        }
+
+        public string getPriceOfPersonMessage()
+        {
+            if (age < 20)//Ungdompris
+            {
+                this.price = (uint)Prices.youth;
+                return $"Ungdompris: {price:C}";
+            }
+            else if (age > 64)//Pensionärspris
+            {
+                this.price = (uint)Prices.senior;
+                return $"Pensionärspris: {price:C}";
+            }
+            else //standardpris
+            {
+                this.price = (uint)Prices.standard;
+                return $"Standardpris: {price:C}";
+            }
+        }
+
+    }
+}
