540df619833cfa16ca4f423883e317cc3bce2075
patrik <patrik@DESKTOP-9ABGC4G>
2024-03-01 09:27:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise2_LoopsAndStringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
